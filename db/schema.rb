6704b8204ae5f8df628822c01e37362a6ac56577
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-01-06 12:27:18 -0500

    Update author fields with user_id
    Add user_id to posts and comments tables
    Add user associations in post and user models
    Add user_id to params in controllers
    Replace author with user_id in html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106152255) do
+ActiveRecord::Schema.define(version: 20180106161912) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180106152255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id"
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -26,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180106152255) do
     t.text "blog_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
